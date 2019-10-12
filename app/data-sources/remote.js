264929603d454d19099286a374e1140c7b55f1c1
Miguel Camba <miguel.camba@gmail.com>
2019-10-13 00:28:23 +0200

    Backup source working. All is blocking

--- app/data-sources/remote.js
+++ app/data-sources/remote.js
@@ -3,6 +3,7 @@ import SourceClass from '@orbit/jsonapi';
 export default {
   create(injections = {}) {
     injections.name = 'remote';
+    injections.defaultFetchSettings = { timeout: 10000 }
     return new SourceClass(injections);
   }
 };
