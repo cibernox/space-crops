174c2bee47db4c83b97ed59a92a382d1459a8f11
Miguel Camba <miguel.camba@gmail.com>
2019-10-13 00:12:41 +0200

    Initial state. Memory only

--- app/data-sources/remote.js
+++
@@ -1,9 +0,0 @@
-import SourceClass from '@orbit/jsonapi';
-
-export default {
-  create(injections = {}) {
-    injections.name = 'remote';
-    injections.defaultFetchSettings = { timeout: 10000 }
-    return new SourceClass(injections);
-  }
-};
